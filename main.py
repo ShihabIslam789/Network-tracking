ce39d7a1467c1121306d63ed115f2060ba2b4756
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-18 13:55:33 -0400

    extract ip data.

--- main.py
+++ main.py
@@ -5,6 +5,31 @@ import pygeoip
 gi = pygeoip.GeoIP('GeoLiteCity.dat')
 
 
+
+def retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name('x.xxx.xxx.xxx')
+    try:
+        dstlongitude = dst['longitude']
+        dstlatitude = dst['latitude']
+        srclongitude = src['longitude']
+        srclatitude = src['latitude']
+        kml = (
+            '<Placemark>\n'
+            '<name>%s</name>\n'
+            '<extrude>1</extrude>\n'
+            '<tessellate>1</tessellate>\n'
+            '<styleUrl>#transBluePoly</styleUrl>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '</LineString>\n'
+            '</Placemark>\n'
+        )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+        return kml
+    except:
+        return ''
+
+
 def plotIPs(pcap):
     kmlPts = ''
     for (ts, buf) in pcap:
